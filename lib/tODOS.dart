8beac1520505f343a51b08d6e250fc8427f0e805
ChoiDoyle <swoo0509@hanyang.ac.kr>
2022-01-13 20:18:30 +0900

    210113Starting

--- lib/tODOS.dart
+++ lib/tODOS.dart
@@ -1,5 +1,5 @@
 //TODOS: firebase RTDB에서 왜 4개 항목까지 밖에 안되는지? 그 이상 넘어가니 index error..
-//TODOS: paymentdialogfunc에서 paid처리되면 firestore에 저장하기
+//TODOS: paymentdialogfunc에서 paid처리되면 FutureBuilder reload 하기
 
 //TODOS: 오늘도
 //TODOS: 화이팅!
